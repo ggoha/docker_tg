25ad8767234253cfc895345cf591428f9d17310b
ggoha <ryznikov@mail.ru>
2019-08-17 02:54:14 +0300

    pelpathyty

--- app/controllers/test_telegram_controller.rb
+++ app/controllers/test_telegram_controller.rb
@@ -2,12 +2,6 @@ class TestTelegramController < Telegram::Bot::UpdatesController
   # use callbacks like in any other controllers
   around_action :with_locale
 
-  def message(message)
-    # message can be also accessed via instance method
-    message == self.payload # true
-    # store_message(message['text'])
-  end
-
   def start!(data = nil, *)
     # do_smth_with(data)
 
@@ -18,17 +12,56 @@ class TestTelegramController < Telegram::Bot::UpdatesController
     respond_with :message, text: response
   end
 
-  private
+  def message(value)
+    return unless value == "Выложить"
 
-  def with_locale(&block)
-    I18n.with_locale(locale_for_update, &block)
+    if party[from["id"]].pop > @@min
+      @@min = party[from["id"]].pop
+      win if party.all? {|_key, value| value.empty? }
+    else
+      lose
+    end
   end
 
-  def locale_for_update
-    if from
-      # locale for user
-    elsif chat
-      # locale for chat
+  def join(*)
+    party[from['id']] = [] 
+  end
+
+  def go(*)
+    if party.count < 1
+      respond_with :message, text: "Недостаточно игроков"
+    else
+      init
     end
+  end
+
+  private
+
+  def init
+    cards = (1..100).to_a.shuffle.each_slice(3).to_a
+    keyboard = { keyboard: ['Выложить'], resize_keyboard: true }
+    party.each_with_index do |(key, value), index|
+      value = cards[index].sort.reverse
+      Telegram.bots[:default].send_message(chat_id: key, text: "Ты получил #{value}", reply_markup: keyboard)
+    end
+    @@min = 0
+  end
+
+  def win
+    party.each do |key, _value|
+      Telegram.bots[:default].send_message(chat_id: key, text: "Вы выиграли")
+    end
+    @@party = {}
+  end
+
+  def lose
+    party.each do |key, _value|
+      Telegram.bots[:default].send_message(chat_id: key, text: "Вы проиграли")
+    end
+    @@party = {}
+  end
+
+  def party
+    @@party ||= {}
   end
 end
